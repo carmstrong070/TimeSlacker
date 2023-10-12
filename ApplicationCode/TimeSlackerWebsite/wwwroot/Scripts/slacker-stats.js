c3412fb69265a29119423ce80187b7a7fdfa7d47
Christian Armstrong <c.armstrong070@gmail.com>
2023-10-11 22:18:29 -0700

    [InitialDev] Updated recent fails display to replace 100% Compliance display

--- ApplicationCode/TimeSlackerWebsite/wwwroot/Scripts/slacker-stats.js
+++ ApplicationCode/TimeSlackerWebsite/wwwroot/Scripts/slacker-stats.js
@@ -20,11 +20,12 @@ function getRecentFails() {
         dataType: 'json',
         contentType: 'application/json; charset=utf-8',
         success: function (data) {
-            let htmlExpression = "";
-            data.forEach((fail) => {
-                htmlExpression += renderRecentFail(fail);
-            });
-            $("#recent-fails-container").html(htmlExpression);
+            if (data.length > 0) {
+                renderRecentFails(data);
+                $("#compliance-video").hide();
+                $("#compliance-container").hide();
+                $("#recent-fails-container").css("background", "#101216");
+            }
         },
         fail: function (jqXHR, textStatus) {
             alert("Request failed: " + textStatus);
@@ -83,28 +84,29 @@ function getFailsOverTime() {
 
 //#region helpers
 
-function renderRecentFail(fail) {
-    let html = "";
+function renderRecentFails(data) {
+    let htmlExpression = ""
+    data.forEach((fail) => {
+        htmlExpression += "<div class='col-xxl-3 col-xl-4 col-md-6 d-md-flex align-items-stretch mb-3'>";
+        htmlExpression += "    <div class='card h-100 w-100 shadow' data-employeeId='" + fail.employeeId + "'>";
+        htmlExpression += "        <div class='card-body d-md-flex flex-md-column'>";
+        htmlExpression += "            <div class='img-fluid card-img-top mb-3'>";
+        htmlExpression += "                <div class='text-center'>";
+        htmlExpression += "                    <i class='fa-solid fa-user' style='font-size: 5rem;'></i>";
+        htmlExpression += "                </div>";
+        htmlExpression += "            </div>";
+        htmlExpression += "            <h5 class='card-title'>" + fail.firstName + " " + fail.lastName + "</h5>";
+        htmlExpression += "            <ul class='card-text flex-md-fill'>";
+        htmlExpression += "                <li>Total Fails: " + fail.totalFails + "</li>";
+        htmlExpression += "                <li>Most Recent Fail: " + fail.mostRecent + "</li>";
+        htmlExpression += "                <li>Total Fails: " + (fail.failRate * 100).toFixed(2) + "%</li>";
+        htmlExpression += "            </ul>";
+        htmlExpression += "        </div>";
+        htmlExpression += "    </div>";
+        htmlExpression += "</div>";
+    });
 
-    html += "<div class='col-xxl-3 col-xl-4 col-md-6 d-md-flex align-items-stretch mb-3'>";
-    html += "    <div class='card h-100 w-100 shadow' data-employeeId='" + fail.employeeId + "'>";
-    html += "        <div class='card-body d-md-flex flex-md-column'>";
-    html += "            <div class='img-fluid card-img-top mb-3'>";
-    html += "                <div class='text-center'>";
-    html += "                    <i class='fa-solid fa-user' style='font-size: 5rem;'></i>";
-    html += "                </div>";
-    html += "            </div>";
-    html += "            <h5 class='card-title'>" + fail.firstName + " " + fail.lastName + "</h5>";
-    html += "            <ul class='card-text flex-md-fill'>";
-    html += "                <li>Total Fails: " + fail.totalFails + "</li>";
-    html += "                <li>Most Recent Fail: " + fail.mostRecent + "</li>";
-    html += "                <li>Total Fails: " + (fail.failRate * 100).toFixed(2) + "%</li>";
-    html += "            </ul>";
-    html += "        </div>";
-    html += "    </div>";
-    html += "</div>";
-
-    return html;
+    $("#recent-fails-display").html(htmlExpression);
 }
 
 //#endregion
